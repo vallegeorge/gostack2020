5e7cff4774122640e3efe6171b3d8b1b8a4e274b
George do Valle Silva <georges@defensoria.ms.def.br>
2020-06-08 15:14:17 -0400

    final nivel 1

---
+++ mobile/README.md
@@ -0,0 +1,6 @@
+
+--> Listando projetos da API
+yarn add axios
+
+fazer o emulador utilizar o localhost:
+adb reverse tcp:3333 tcp:3333
--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,13 +1,51 @@
-import React from 'react';
-import { View, Text, StyleSheet, StatusBar } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { SafeAreaView, FlatList, Text, StyleSheet, StatusBar, TouchableOpacity } from 'react-native';
+
+import api from './services/api';
 
 export default function App() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get('projects').then(response => {
+      console.log(response.data);
+      setProjects(response.data);
+    })
+  }, []);
+
+  async function handleAddProject() {
+    const response = await api.post('projects', {
+      title: `Frontend ${Date.now()}`,
+      owner: "George"
+    });
+
+    const project = response.data;
+
+    setProjects([...projects, project ]);
+  }
+
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#7159c1"/>
-      <View style={styles.container}>
-        <Text style={styles.title}>Hello GoStack</Text>
-      </View>
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          data={projects}
+          keyExtractor={project => project.id}
+          renderItem={({ item: project }) => (
+            <Text style={styles.project}>{project.title}</Text>
+          )}
+        />
+
+        <TouchableOpacity activeOpacity={0.6} style={styles.button} onPress={handleAddProject}>
+          <Text style={styles.buttonText}>Adicionar projeto</Text>
+        </TouchableOpacity>
+      </SafeAreaView>
+      
+      {/* <View style={styles.container}>
+        {projects.map(project => (
+          <Text style={styles.project} key={project.id}>{project.title}</Text>
+        ))}
+        </View> */}
     </>
   );
 }
@@ -15,14 +53,25 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#7159c1',
-    justifyContent: 'center',
-    alignItems: 'center',
+    backgroundColor: '#7159c1'
   },
 
-  title: {
+  project: {
     color: '#FFF',
-    fontSize: 32,
-    fontWeight: 'bold'
+    fontSize: 20,
+  },
+
+  button: {
+    backgroundColor: '#FFF',
+    margin: 20,
+    height: 50,
+    borderRadius: 4,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  buttonText: {
+    fontWeight: 'bold',
+    fontSize: 16,
   }
 })
